a6484cddee11d4985dac14ae1e652a61ff55e321
khoinguyenkc <khoinguyenkc@gmail.com>
2021-02-28 13:57:47 -0800

    started building calculate_grade method for Course model on Rails side

--- backend/app/models/course.rb
+++ backend/app/models/course.rb
@@ -1,3 +1,20 @@
 class Course < ApplicationRecord
     has_many :categories
+
+    def calculate_grade
+        percentages = []
+        cats = self.categories
+        #loop thru cats, and in there, loop thru assignments, 
+        cats.each do   |cat| {
+            cat_score_sum = 0;
+            cat_out_of_sum = 0;
+            cat.each do | assignment | {
+                #add score
+                #add dsum
+            } 
+            percentage = cat.weight * (cat_score_sum / cat_out_of_sum)
+            percentages.push(percentage)
+        }
+    end
+
 end
